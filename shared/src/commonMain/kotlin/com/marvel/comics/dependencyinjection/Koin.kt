d57977d06014008cadce831bee05abfa01d73426
Matthew Callery <callerymatthew@gmail.com>
2022-03-02 19:31:25 +0000

    Create Koin.kt dependency injection file

---
+++ shared/src/commonMain/kotlin/com/marvel/comics/dependencyinjection/Koin.kt
@@ -0,0 +1,54 @@
+package com.marvel.comics.dependencyinjection
+
+import com.marvel.comics.api.MarvelComicsApi
+import com.marvel.comics.repository.MarvelComicsRepository
+import com.marvel.comics.repository.MarvelComicsRepositoryInterface
+import com.marvel.comics.repository.platformModule
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.serialization.json.Json
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+import org.koin.dsl.module
+
+// Android Koin initialisation
+fun initKoin(enableNetworkLogs: Boolean = false, appDeclaration: KoinAppDeclaration = {}) =
+    startKoin {
+        appDeclaration()
+        modules(commonModule(enableNetworkLogs = enableNetworkLogs), platformModule())
+    }
+
+fun commonModule(enableNetworkLogs: Boolean) = module {
+    single { createJson() }
+    single { createHttpClient(get(), get(), enableNetworkLogs = enableNetworkLogs) }
+
+    single { CoroutineScope(Dispatchers.Default + SupervisorJob()) }
+
+    single<MarvelComicsRepositoryInterface> { MarvelComicsRepository() }
+
+    single { MarvelComicsApi(get()) }
+}
+
+fun createJson() = Json { isLenient = true; ignoreUnknownKeys = true }
+
+fun createHttpClient(httpClientEngine: HttpClientEngine, json: Json, enableNetworkLogs: Boolean) =
+    HttpClient(httpClientEngine) {
+        install(ContentNegotiation) {
+            json(json)
+        }
+        if (enableNetworkLogs) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.INFO
+            }
+        }
+    }
