44c6e777430c5ca7299a81faf61ad18447479d54
Matthew Callery <callerymatthew@gmail.com>
2022-03-02 19:32:04 +0000

    Add dependencies to androidApp build.gradle.kts

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -5,23 +5,86 @@ plugins {
 
 android {
     compileSdk = 31
+
     defaultConfig {
         applicationId = "com.marvel.comics.android"
         minSdk = 21
         targetSdk = 31
         versionCode = 1
         versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0"
+    }
+
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
     implementation(project(":shared"))
+
+    // Android
     implementation("com.google.android.material:material:1.5.0")
+    implementation("org.osmdroid:osmdroid-android:6.1.10")
+
+    // AndroidX
     implementation("androidx.appcompat:appcompat:1.4.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0-alpha03")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.0-alpha03")
+    implementation("androidx.activity:activity-compose:1.5.0-alpha03")
+
+    // Compose
+    implementation("androidx.compose.compiler:compiler:1.1.1")
+    implementation("androidx.compose.ui:ui:1.1.1")
+    implementation("androidx.compose.ui:ui-graphics:1.1.1")
+    implementation("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation("androidx.compose.foundation:foundation-layout:1.1.1")
+    implementation("androidx.compose.material:material:1.1.1")
+    implementation("androidx.navigation:navigation-compose:2.4.1")
+    implementation("io.coil-kt:coil-compose:1.3.1")
+    implementation("com.google.accompanist:accompanist-navigation-animation:0.23.0")
+
+    // Koin
+    implementation("io.insert-koin:koin-core:3.1.4")
+    implementation("io.insert-koin:koin-test:3.1.4")
+    implementation("io.insert-koin:koin-test-junit4:3.1.4")
+    implementation("io.insert-koin:koin-android:3.1.4")
+    implementation("io.insert-koin:koin-androidx-compose:3.1.4")
+
+    // Testing
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.test:core:1.4.0")
+    testImplementation("org.robolectric:robolectric:4.6.1")
+    testImplementation("org.mockito:mockito-inline:3.11.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+
+    // Compose testing
+    androidTestImplementation("androidx.compose.ui:ui-test:1.1.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.1")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.1.1")
 }
