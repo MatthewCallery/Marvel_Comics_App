66b9158a9af60f691993d749f2151175e2fbc0ec
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 13:07:56 +0000

    Add fetchComicDataWrapper method to MarvelComicsApi.kt

--- shared/src/commonMain/kotlin/com/marvel/comics/api/MarvelComicsApi.kt
+++ shared/src/commonMain/kotlin/com/marvel/comics/api/MarvelComicsApi.kt
@@ -1,9 +1,11 @@
 package com.marvel.comics.api
 
 import com.marvel.comics.BuildKonfig
+import com.marvel.comics.api.serializable.ComicDataWrapper
 import com.marvel.comics.api.serializable.MarvelCharacterDataWrapper
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import kotlinx.datetime.Clock
@@ -14,14 +16,24 @@ class MarvelComicsApi(
     private val baseUrl: String = "https://gateway.marvel.com/v1/public"
 ) : KoinComponent {
 
-    suspend fun fetchCharacterDataWrapper(): MarvelCharacterDataWrapper {
-        val timestamp = Clock.System.now().toEpochMilliseconds().toString()
-
-        return client.get("$baseUrl/characters") {
-            parameter("apikey", BuildKonfig.publicMarvelApiKey)
-            parameter("ts", timestamp)
-            parameter("hash", md5Hash(timestamp = timestamp))
-            parameter("limit", 100)
+    suspend fun fetchCharacterDataWrapper(): MarvelCharacterDataWrapper =
+        client.get("$baseUrl/characters") {
+            addMarvelApiParams()
         }.body()
+
+    suspend fun fetchComicDataWrapper(characterId: Int): ComicDataWrapper =
+        client.get("$baseUrl/characters/$characterId/comics") {
+            addMarvelApiParams()
+        }.body()
+
+    private fun HttpRequestBuilder.addMarvelApiParams() {
+        val timestamp = generateTimestamp()
+
+        parameter("apikey", BuildKonfig.publicMarvelApiKey)
+        parameter("ts", timestamp)
+        parameter("hash", md5Hash(timestamp = timestamp))
+        parameter("limit", 100)
     }
+
+    private fun generateTimestamp(): String = Clock.System.now().toEpochMilliseconds().toString()
 }
