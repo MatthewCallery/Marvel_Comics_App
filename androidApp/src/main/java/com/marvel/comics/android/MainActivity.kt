253cf769b0bccb9a5e151550ed48e17e4d487935
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 11:11:28 +0000

    Update MainActivity.kt to show CharacterListScreen on app open

--- androidApp/src/main/java/com/marvel/comics/android/MainActivity.kt
+++ androidApp/src/main/java/com/marvel/comics/android/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.marvel.comics.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.marvel.comics.Greeting
-import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import coil.annotation.ExperimentalCoilApi
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.marvel.comics.android.navigation.Screen
+import com.marvel.comics.android.navigation.addCharacterListScreen
+import com.marvel.comics.android.theme.MarvelComicsTheme
 
-fun greet(): String {
-    return Greeting().greeting()
-}
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            MainLayout()
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun MainLayout() {
+    val navController = rememberAnimatedNavController()
+
+    MarvelComicsTheme {
+        Scaffold { paddingValues ->
+            AnimatedNavHost(navController, startDestination = Screen.CharacterList.title) {
+                this.addCharacterListScreen(
+                    navController = navController,
+                    paddingValues = paddingValues
+                )
+            }
+        }
     }
 }
