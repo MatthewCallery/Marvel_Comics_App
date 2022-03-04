344c9a16f73c8362c78b1392bf31ef120349b3ba
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 11:08:29 +0000

    Create CharacterItemView.kt for marvel character list screen

---
+++ androidApp/src/main/java/com/marvel/comics/android/ui/CharacterData.kt
@@ -0,0 +1,18 @@
+package com.marvel.comics.android.ui
+
+import androidx.compose.ui.tooling.preview.datasource.CollectionPreviewParameterProvider
+import com.marvel.comics.api.serializable.MarvelCharacter
+
+/**
+ * Dummy data for the Jetpack Compose preview of CharacterItemView
+ */
+class CharacterProvider : CollectionPreviewParameterProvider<MarvelCharacter>(
+    listOf(
+        MarvelCharacter(
+            id = 1,
+            name = "Spiderman",
+            description = "Friendly neighborhood Spiderman"
+        ),
+        MarvelCharacter(id = 2, name = "Thor", description = "God of Thunder")
+    )
+)
---
+++ androidApp/src/main/java/com/marvel/comics/android/ui/CharacterItemView.kt
@@ -0,0 +1,93 @@
+package com.marvel.comics.android.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
+import com.marvel.comics.android.R
+import com.marvel.comics.android.theme.MarvelComicsTheme
+import com.marvel.comics.api.serializable.MarvelCharacter
+
+@ExperimentalCoilApi
+@Composable
+fun CharacterItemView(
+    character: MarvelCharacter,
+    onCharacterSelected: (character: MarvelCharacter) -> Unit
+) {
+
+    val characterName = character.name ?: stringResource(id = R.string.warning_name_not_found)
+    val fallbackDescription =
+        String.format(stringResource(id = R.string.warning_descript_not_found), characterName)
+    val characterDescription =
+        if (character.description.isNullOrEmpty()) fallbackDescription
+        else character.description ?: fallbackDescription
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = { onCharacterSelected(character) })
+            .padding(dimensionResource(id = R.dimen.padding_m)),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        CharacterItemViewImage(
+            characterImageUrl = character.imageUrl ?: "",
+            contentDescription = characterName
+        )
+
+        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.char_item_view_spacer)))
+
+        Column {
+            Text(text = characterName, style = MaterialTheme.typography.h2)
+            Text(
+                text = characterDescription,
+                style = MaterialTheme.typography.subtitle2.copy(color = Color.DarkGray),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@ExperimentalCoilApi
+@Composable
+fun CharacterItemViewImage(characterImageUrl: String, contentDescription: String) {
+    if (characterImageUrl.isNotEmpty()) {
+        Image(
+            painter = rememberImagePainter(characterImageUrl),
+            modifier = Modifier
+                .clip(RoundedCornerShape(dimensionResource(id = R.dimen.image_corner_radius)))
+                .size(dimensionResource(id = R.dimen.image_size_small)),
+            contentDescription = contentDescription
+        )
+    } else {
+        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.image_size_small)))
+    }
+}
+
+@ExperimentalCoilApi
+@Preview
+@Composable
+fun DefaultPreview(@PreviewParameter(CharacterProvider::class) character: MarvelCharacter) {
+    MarvelComicsTheme {
+        CharacterItemView(character, onCharacterSelected = {})
+    }
+}
