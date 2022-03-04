6d615893871baef7d55837304f518a97325992b4
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 12:48:51 +0000

    Add elevation to CharacterDetailHeaderView.kt and clean up

--- androidApp/src/main/java/com/marvel/comics/android/ui/CharacterDetailHeaderView.kt
+++ androidApp/src/main/java/com/marvel/comics/android/ui/CharacterDetailHeaderView.kt
@@ -7,13 +7,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.text.style.TextOverflow
 import coil.annotation.ExperimentalCoilApi
 import com.marvel.comics.android.R
 import com.marvel.comics.api.serializable.MarvelCharacter
@@ -25,29 +25,31 @@ fun CharacterDetailHeaderView(character: MarvelCharacter) {
     val characterName = character.name ?: ""
     val characterDescription = character.description ?: ""
 
-    Column {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            CharacterImageView(
-                characterImageUrl = character.imageUrl,
-                imageSize = dimensionResource(id = R.dimen.image_size_large),
-                contentDescription = characterName
-            )
+    Surface(elevation = dimensionResource(id = R.dimen.elevation_card)) {
+        Column(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_m))) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                CharacterImageView(
+                    characterImageUrl = character.imageUrl,
+                    imageSize = dimensionResource(id = R.dimen.image_size_large),
+                    contentDescription = characterName
+                )
 
-            Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_m)))
+                SpacerMedium()
 
-            Text(text = characterName, style = MaterialTheme.typography.h1)
-        }
+                Text(text = characterName, style = MaterialTheme.typography.h1)
+            }
 
-        Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_m)))
+            if (characterDescription.isNotEmpty()) {
+                SpacerMedium()
 
-        if (characterDescription.isNotEmpty()) {
-            Text(
-                text = characterDescription,
-                style = MaterialTheme.typography.subtitle1.copy(color = Color.DarkGray)
-            )
+                Text(
+                    text = characterDescription,
+                    style = MaterialTheme.typography.subtitle1.copy(color = Color.DarkGray)
+                )
+            }
         }
     }
 }
