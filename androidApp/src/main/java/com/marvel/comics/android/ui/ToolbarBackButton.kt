b2d3afd0e13f3fb34218ac3a7090226401368cc7
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 12:34:26 +0000

    Create ToolbarBackButton.kt

---
+++ androidApp/src/main/java/com/marvel/comics/android/ui/ToolbarBackButton.kt
@@ -0,0 +1,19 @@
+package com.marvel.comics.android.ui
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.marvel.comics.android.R
+
+@Composable
+fun ToolbarBackButton(onClickBack: () -> Unit) {
+    IconButton(onClick = { onClickBack() }) {
+        Icon(
+            Icons.Filled.ArrowBack,
+            contentDescription = stringResource(id = R.string.nav_back)
+        )
+    }
+}
