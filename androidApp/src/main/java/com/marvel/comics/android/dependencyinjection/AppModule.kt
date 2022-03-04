4aa294c850da24f8ae5b75368ca7b6ec33b26b96
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 10:58:34 +0000

    Add documentation to AppModule.kt

--- androidApp/src/main/java/com/marvel/comics/android/dependencyinjection/AppModule.kt
+++ androidApp/src/main/java/com/marvel/comics/android/dependencyinjection/AppModule.kt
@@ -4,6 +4,9 @@ import com.marvel.comics.android.viewmodel.MarvelComicsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+/**
+ * Dependency injection for the Android app
+ */
 val appModule = module {
     viewModel { MarvelComicsViewModel(get()) }
 }
