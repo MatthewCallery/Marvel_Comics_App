eeffe421e4cfe9eac889bf673d2361bd96014d70
Matthew Callery <callerymatthew@gmail.com>
2022-03-04 12:47:32 +0000

    Create Spacers.kt for common Spacer elements

---
+++ androidApp/src/main/java/com/marvel/comics/android/ui/Spacers.kt
@@ -0,0 +1,13 @@
+package com.marvel.comics.android.ui
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.marvel.comics.android.R
+
+@Composable
+fun SpacerMedium() {
+    Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_m)))
+}
