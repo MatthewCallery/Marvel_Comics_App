372ade3b3315a296adedc0ed65cb89d641689602
Matthew Callery <callerymatthew@gmail.com>
2022-03-06 13:52:56 +0000

    Add border and placeholder to CharacterImageView.kt

--- androidApp/src/main/java/com/marvel/comics/android/ui/components/CharacterImageView.kt
+++ androidApp/src/main/java/com/marvel/comics/android/ui/components/CharacterImageView.kt
@@ -1,11 +1,11 @@
 package com.marvel.comics.android.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -24,17 +24,25 @@ fun CharacterImageView(
     contentDescription: String
 ) {
     val safeImageUrl = characterImageUrl ?: ""
-
-    if (safeImageUrl.isNotEmpty()) {
-        Image(
-            painter = rememberImagePainter(safeImageUrl),
-            modifier = Modifier
-                .clip(RoundedCornerShape(dimensionResource(id = R.dimen.image_corner_radius)))
-                .height(imageHeight)
-                .width(imageWidth),
-            contentDescription = contentDescription
+    val imageShape = RoundedCornerShape(dimensionResource(id = R.dimen.image_corner_radius))
+    val imageModifier = Modifier
+        .clip(imageShape)
+        .height(imageHeight)
+        .width(imageWidth)
+        .border(
+            width = dimensionResource(id = R.dimen.image_border_width),
+            color = MaterialTheme.colors.onSurface,
+            shape = imageShape
         )
-    } else {
-        Spacer(modifier = Modifier.size(imageHeight))
-    }
+
+    Image(
+        painter = rememberImagePainter(
+            safeImageUrl,
+            builder = {
+                placeholder(R.drawable.default_character)
+            }
+        ),
+        modifier = imageModifier,
+        contentDescription = contentDescription,
+    )
 }
